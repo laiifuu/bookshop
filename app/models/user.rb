a502d96c906efd34af4028ac7fd08c300e329a8f
laiifuu <hayet.fellah145@gmail.com>
2023-11-03 18:58:09 +0100

    Setup login and registration with jwt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ApplicationRecord
+  include Devise::JWT::RevocationStrategies::JTIMatcher
+
   has_many :reviews 
   has_many :orders
   
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :validatable, 
+         :jwt_authenticatable, jwt_revocation_strategy: self
+
 end
